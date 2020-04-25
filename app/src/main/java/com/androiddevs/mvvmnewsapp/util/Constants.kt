615a1faedf70774db378567dccfc7ac15e53bfb5
siddhant0912 <siddhantarekar12@gmail.com>
2020-04-25 17:12:27 +0530

    intial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY ="65bc69bff39c4ca8b34f26ee72c2509c"
+        const val BASE_URL ="https://newsapi.org"
+    }
+}
