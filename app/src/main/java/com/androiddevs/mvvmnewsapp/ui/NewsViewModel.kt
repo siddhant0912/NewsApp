f28a3a1cba2e7d12d62cb10cfb705e12263e1283
siddhant0912 <siddhantarekar12@gmail.com>
2020-05-05 20:28:12 +0530

    Pagination Added

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -14,9 +14,13 @@ import retrofit2.Response
 class NewsViewModel(val newsRepository : NewsRepository):ViewModel(){
 
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-    val breakingNewsPage = 1
+    var breakingNewsPage = 1
+    var breakingNewsResponse:NewsResponse ?= null
+
     val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-    val searchNewsPage =1
+    var searchNewsPage =1
+    var searchNewsResponse: NewsResponse ?= null
+
 
     init{
         getBreakingNews("in")
@@ -37,7 +41,15 @@ class NewsViewModel(val newsRepository : NewsRepository):ViewModel(){
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>):Resource<NewsResponse> {
         if (response.isSuccessful) {
             response.body()?.let { resultResponse ->
-                return Resource.Success(resultResponse)
+                breakingNewsPage++
+                if(breakingNewsResponse == null){
+                    breakingNewsResponse = resultResponse
+                }else{
+                    val oldArticles = breakingNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(breakingNewsResponse ?: resultResponse)
             }
         }
         return  Resource.Error(response.message())
@@ -46,7 +58,15 @@ class NewsViewModel(val newsRepository : NewsRepository):ViewModel(){
     private fun handleSearchNewsResponse(response: Response<NewsResponse>):Resource<NewsResponse> {
         if (response.isSuccessful) {
             response.body()?.let { resultResponse ->
-                return Resource.Success(resultResponse)
+                searchNewsPage++
+                if(searchNewsResponse == null){
+                    searchNewsResponse = resultResponse
+                }else{
+                    val oldArticles = searchNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(searchNewsResponse ?: resultResponse)
             }
         }
         return  Resource.Error(response.message())
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY ="65bc69bff39c4ca8b34f26ee72c2509c"
         const val BASE_URL ="https://newsapi.org"
         const val SEARCH_NEWS_DELAY = 500L
+        const val  QUERY_PAGE_SIZE= 20
     }
 }
